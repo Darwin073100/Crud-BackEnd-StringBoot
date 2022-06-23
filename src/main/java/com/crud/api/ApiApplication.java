38b58c44a0d5381ea23a89e51cee3d53b459d57f
Darwin073100 <edwinegq@outlook.com>
2022-06-23 08:12:30 -0500

    Preparado para despliegue en heroku

--- src/main/java/com/crud/api/ApiApplication.java
+++ src/main/java/com/crud/api/ApiApplication.java
@@ -21,7 +21,7 @@ public class ApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080")
+						.allowedOrigins("*")
 						.allowedMethods("*").allowedHeaders("*");
 			}
 		};
